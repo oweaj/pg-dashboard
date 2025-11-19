364524798e615d10f5bb39e056bdd7ec4ceeaad9
oweaj <qwe1520156@naver.com>
2025-11-19 21:37:03 +0900

    fix: 날짜 포멧 유틸 수정

--- src/utils/dateFormet.ts
+++ src/utils/dateFormet.ts
@@ -1,6 +1,9 @@
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 
-export const dateFormat = (date: Date, type = "yyyy.MM.dd EEEE") => {
-  return date ? format(date, type, { locale: ko }) : "";
+export const dateFormat = (date: Date | string, option: { day: boolean }) => {
+  if (typeof date === "string") new Date(date);
+  const formatDate = option.day ? "yyyy.MM.dd EEEE" : "yyyy.MM.dd";
+
+  return format(date, formatDate, { locale: ko });
 };
