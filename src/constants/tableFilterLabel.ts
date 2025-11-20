98e79605146085bfc39f5939331baf0a2ea6d3e5
oweaj <qwe1520156@naver.com>
2025-11-21 07:30:29 +0900

    fix: 상수 데이터 수정

--- src/constants/tableFilterLabel.ts
+++ src/constants/tableFilterLabel.ts
@@ -4,13 +4,11 @@ export const TABLE_FILTER_LABEL = {
     status: "status",
     phType: "결제수단",
     phStatus: "상태",
-    addBtn: "거래내역 추가",
   },
   merchant: {
     typeId: "bizType",
     status: "status",
-    phType: "가맹분류",
+    phType: "가맹유형",
     phStatus: "상태",
-    addBtn: "가맹점 추가",
   },
 } as const;
