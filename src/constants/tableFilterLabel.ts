bd85c402667bd838847f284369b8ad63ccbdbe83
oweaj <qwe1520156@naver.com>
2025-11-20 18:31:43 +0900

    chore: 페이지 별 공통 테이블 컴포넌트 라벨 상수 데이터 설정

---
+++ src/constants/tableFilterLabel.ts
@@ -0,0 +1,16 @@
+export const TABLE_FILTER_LABEL = {
+  payment: {
+    typeId: "payType",
+    status: "status",
+    phType: "결제수단",
+    phStatus: "상태",
+    addBtn: "거래내역 추가",
+  },
+  merchant: {
+    typeId: "bizType",
+    status: "status",
+    phType: "가맹분류",
+    phStatus: "상태",
+    addBtn: "가맹점 추가",
+  },
+} as const;
