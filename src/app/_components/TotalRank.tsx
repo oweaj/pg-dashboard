3d1a798dcd8b81874234e8c815961f3c3d735e56
oweaj <qwe1520156@naver.com>
2025-11-21 07:55:17 +0900

    feat: 거리내역 중 금액순 랭크 생성

--- src/app/_components/TotalRank.tsx
+++ src/app/_components/TotalRank.tsx
@@ -1,8 +1,9 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { useMerchantList } from "@/hooks/useMerchant";
 import type { IPaymentListType } from "@/types/payment.type";
 import { useMemo } from "react";
 
+// 거래 금액 랭킹 컴포넌트
 const TotalRank = ({ data }: { data: IPaymentListType[] }) => {
   const { data: merchantList } = useMerchantList();
 
@@ -19,21 +20,25 @@ const TotalRank = ({ data }: { data: IPaymentListType[] }) => {
   }, [data, merchantList]);
 
   return (
-    <Card className="md:col-span-1">
-      <CardHeader>
+    <Card className="flex-1">
+      <CardHeader className="text-center mb-2">
         <CardTitle>거래별 금액 TOP5</CardTitle>
+        <CardDescription className="text-sm">※ 단순 거래별 금액 순위입니다</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="space-y-7 xl:space-y-10">
+        <div className="space-y-7 xl:space-y-12">
           {topFiveAmount.map((item, i) => (
-            <div key={item.mchtCode} className="flex flex-col justify-between xl:flex-row xl:items-center">
+            <div
+              key={item.paymentCode}
+              className="w-full flex flex-row items-center justify-evenly md:justify-between md:flex-col xl:flex-row xl:items-center"
+            >
               <div className="flex items-center gap-3">
                 <span className="w-7 h-7 flex items-center justify-center bg-primary rounded-md text-xs text-white font-semibold">
                   {i + 1}
                 </span>
                 <span className="text-sm">{item.name}</span>
               </div>
-              <span className="text-sm font-medium ml-[41px] xl:ml-0">{Number(item.amount).toLocaleString()}원</span>
+              <span className="text-sm ml-1 font-medium">{Number(item.amount).toLocaleString()}원</span>
             </div>
           ))}
         </div>
